6abee1d821ba91671d27eda403770db6bc374017
Roberto <robertofigueiredo2000@gmail.com>
2020-02-19 22:34:17 +0000

    file sorting

--- robocup17-gui-master/src/DefinitionHolder.kt
+++
@@ -1,8 +0,0 @@
-class DefinitionHolder {
-
-    val environments = mutableListOf<Environment>()
-    val superobjectRules = mutableListOf<SuperobjectRule>()
-    val walls = mutableListOf<Wall>()
-    val areaGroups = mutableListOf<AreaGroup>()
-
-}
--- robocup17-gui-master/src/ValueTag.kt
+++
@@ -1,2 +0,0 @@
-class ValueTag<T>(val obj: T, val value: Double)
-class DoubleValueTag<T, U>(val list: MutableList<T>, val obj: U, val value: Double)
